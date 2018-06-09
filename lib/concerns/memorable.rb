af3f276c02f2696cb853f9c274f83b99ef528eaa
Christia T <christiatidwell@bellsouth.net>
2018-06-09 20:58:38 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,15 +1,20 @@
 module Memorable
 
-@@artists = []
+  module ClassMethods
+      def reset_all
+        self.all.clear
+      end
 
+      def count
+        self.all.count
+      end
+    end
 
-  def reset_all
-    all.clear
-  end
-
-  def count
-    @@artists.count
-  end
+    module InstanceMethods
+      def initialize
+        self.class.all << self
+      end
+    end
 
 
 end
